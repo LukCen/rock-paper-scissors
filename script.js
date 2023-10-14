06b708846106478eabe68c47bd12a9268e59d89e
Lukasz Cena <lbjcena@gmail.com>
2023-10-14 17:47:32 +0200

    base game logic
    base game logic implemented, results for player choosing rock added

--- script.js
+++ script.js
@@ -4,4 +4,28 @@ function getComputerChoice(){
   return choices[randomIndex]
 }
 
-console.log(getComputerChoice())
+function playerSelection(){
+  return prompt('What would your selection be?').toLowerCase()
+}
+
+
+function playRound(getPlayerChoice, getComputerChoice) {
+  let playerWins = 0;
+  let computerWins = 0;
+  let draws = 0;
+  
+  if(getPlayerChoice === 'rock') {
+    if(getComputerChoice === getPlayerChoice) {
+      draws++
+      console.log(`A draw! You picked ${getPlayerChoice}, the computer picked ${getComputerChoice}`)
+    } else if (getComputerChoice === 'paper') {
+      computerWins++
+      console.log(`You lost! You picked ${getPlayerChoice}, the computer picked ${getComputerChoice}`)
+    } else {
+      playerWins++
+      console.log(`You win! You picked ${getPlayerChoice}, the computer picked ${getComputerChoice}`)
+    }
+  }
+}
+
+console.log(playRound(playerSelection(), getComputerChoice()))
