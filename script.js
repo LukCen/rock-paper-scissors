e8834b87564c51d7a8c824e7f7e4851fd0f22b7f
Lukasz Cena <lbjcena@gmail.com>
2023-10-18 21:16:02 +0200

    first iteration - probably function scope issues, refactor

--- script.js
+++ script.js
@@ -1,8 +1,76 @@
-// function getComputerChoice(){
-//   const choices = ['rock', 'paper', 'scissors']
-//   let randomIndex = Math.floor(Math.random() * 3)
-//   return choices[randomIndex]
-// }
+'use strict'
+const btnRock = document.querySelector('.select-rock')
+const btnPaper = document.querySelector('.select-paper')
+const btnScissors = document.querySelector('.select-scissors')
+
+function handleScore(){
+  let playerWins = 0;
+  let computerWins = 0;
+  let draws = 0;
+
+  function playerVictory(){
+    playerWins++
+    console.log('PLAYER WON')
+  }
+  function computerVictory(){
+    computerWins++
+    console.log('COMPUTER WON')
+  }
+  function draw(){
+    draws++
+    console.log('DRAW')
+  }
+  return {playerWins, computerWins, draws, playerVictory, computerVictory, draw} // need this to access those outside of this function, because scope
+}
+
+function getComputerChoice(){
+  const choices = ['rock', 'paper', 'scissors']
+  let randomIndex = Math.floor(Math.random() * 3)
+  console.log(choices[randomIndex])
+  return choices[randomIndex]
+}
+
+function getPlayerChoice(){
+  let playerChoice = null;
+  btnRock.addEventListener('click', () => {
+    playerChoice = 'rock'
+    console.log(playerChoice)
+    return playerChoice
+  })
+  btnPaper.addEventListener('click', () => {
+    playerChoice = 'paper'
+    console.log(playerChoice)
+    return playerChoice
+  })
+  btnScissors.addEventListener('click', () => {
+    playerChoice = 'scissors'
+    console.log(playerChoice)
+    return playerChoice
+  })
+
+  return {playerChoice}
+}
+
+function playGame() {
+  let trackScore = handleScore()
+  let player = getComputerChoice()
+  let pc = getPlayerChoice()
+
+  if(player === 'rock') {
+    if (pc === player) {
+      trackScore.draw()
+    } else if (pc === 'paper') {
+      trackScore.computerVictory()
+    } else if (pc === 'scissors'){
+      trackScore.playerVictory()
+    }
+  }
+  return trackScore
+}
+
+playGame()
+
+
 
 // function playerSelection(){
 //   return prompt('What would your selection be?').toLowerCase()
