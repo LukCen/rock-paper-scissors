dddd17b7654f127264a4443524a7f5c4b775813c
Lukasz Cena <lbjcena@gmail.com>
2023-10-20 09:11:30 +0200

    game logic completed

--- README.md
+++ README.md
@@ -2,17 +2,16 @@ Rock-paper-scissors project, based on an assignment by TheOdinProject.
 
 Resources :
 
-Rock : <https://www.flaticon.com/free-icons/fist> - Fist icons created by Cuputo - Flaticon
-=============================================================================================
-Paper : <https://www.flaticon.com/free-icon/hand-paper_12355903> - Rock paper scissors icons created by iconading - Flaticon
-=============================================================================================
-Scissors : <https://www.flaticon.com/free-icons/scissors"> - Scissors icons created by Cap Cool - Flaticon
+Rock : <https://www.flaticon.com/free-icons/fist> - Fist icons created by Cuputo - Flaticon<br>
+Paper : <https://www.flaticon.com/free-icon/hand-paper_12355903> - Rock paper scissors icons created by iconading - Flaticon<br>
+Scissors : <https://www.flaticon.com/free-icons/scissors"> - Scissors icons created by Cap Cool - Flaticon<br>
 
 Favicon : 
 
 This favicon was generated using the following font:
   <https://www.favicon.io>
-- Font Title: Leckerli One
-- Font Author: Copyright (c) 2011 Gesine Todt (www.gesine-todt.de hallo@gesine-todt.de), with Reserved Font Names "Leckerli"
-- Font Source: http://fonts.gstatic.com/s/leckerlione/v20/V8mCoQH8VCsNttEnxnGQ-1itLZxcBtItFw.ttf
-- Font License: SIL Open Font License, 1.1 (http://scripts.sil.org/OFL))
+
+- Font Title: Leckerli One<br>
+- Font Author: Copyright (c) 2011 Gesine Todt (www.gesine-todt.de hallo@gesine-todt.de), with Reserved Font Names "Leckerli"<br>
+- Font Source: http://fonts.gstatic.com/s/leckerlione/v20/V8mCoQH8VCsNttEnxnGQ-1itLZxcBtItFw.ttf<br>
+- Font License: SIL Open Font License, 1.1 (http://scripts.sil.org/OFL))<br>
--- script.js
+++ script.js
@@ -2,12 +2,17 @@
 const btnRock = document.querySelector('.select-rock')
 const btnPaper = document.querySelector('.select-paper')
 const btnScissors = document.querySelector('.select-scissors')
+const scorePlayer = document.querySelector('.player')
+const scoreComputer = document.querySelector('.computer')
+const scoreDraw = document.querySelector('.draw')
+
+// I don't like the idea of keeping these variables in global scope but it'll have to do for now as I have no ideas about this
+// and i dont want to cram everything into a single function
+let playerWins = 0
+let computerWins = 0
+let draws = 0
 
 function handleScore () {
-  let playerWins = 0
-  let computerWins = 0
-  let draws = 0
-
   function playerVictory () {
     playerWins++
   }
@@ -17,7 +22,7 @@ function handleScore () {
   function draw () {
     draws++
   }
-  return { playerWins, computerWins, draws, playerVictory, computerVictory, draw } // need this to access those outside of this function, because scope
+  return { playerWins, computerWins, draws, playerVictory, computerVictory, draw } // need this to access those outside of this function, because scope (I think)
 }
 
 function getComputerChoice () {
@@ -49,30 +54,39 @@ function playGame () {
     if (player === 'rock') {
       if (getComputerChoice() === player) {
         handleScore().draw()
+        scoreDraw.innerText = `Draws : ${handleScore().draws}`
       } else if (getComputerChoice() === 'paper') {
         handleScore().computerVictory()
+        scoreComputer.innerText = `Computer : ${handleScore().computerWins}`
       } else {
         handleScore().playerVictory()
+        scorePlayer.innerText = `Computer : ${handleScore().playerWins}`
       }
     }
 
     if (player === 'paper') {
       if (getComputerChoice() === player) {
         handleScore().draw()
+        scoreDraw.innerText = `Draws : ${handleScore().draws}`
       } else if (getComputerChoice() === 'scissors') {
         handleScore().computerVictory()
+        scoreComputer.innerText = `Computer : ${handleScore().computerWins}`
       } else {
         handleScore().playerVictory()
+        scorePlayer.innerText = `Player : ${handleScore().playerWins}`
       }
     }
 
     if (player === 'scissors') {
       if (getComputerChoice() === player) {
         handleScore().draw()
+        scoreDraw.innerText = `Draws : ${handleScore().draws}`
       } else if (getComputerChoice() === 'rock') {
         handleScore().computerVictory()
+        scoreComputer.innerText = `Computer : ${handleScore().computerWins}`
       } else {
         handleScore().playerVictory()
+        scorePlayer.innerText = `Player : ${handleScore().playerWins}`
       }
     }
   })
